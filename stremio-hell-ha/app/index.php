11da70d4ccf598e1c08e361a037a500a5a01965e
Filip <filin.raska@email.cz>
2026-02-09 12:53:04 +0100

    Remove CORS headers from index.php

--- stremio-hell-ha/app/index.php
+++ stremio-hell-ha/app/index.php
@@ -5,10 +5,6 @@
 // and normalize REQUEST_URI so that
 // /manifest.json and /stream work correctly.
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-
 if (($_SERVER['REQUEST_METHOD'] ?? '') === 'OPTIONS') {
     http_response_code(204);
     exit;
